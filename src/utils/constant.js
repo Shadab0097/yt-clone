6fdc3447b46ad697243dba5cfbc0b1483b4e1f5e
shadab khan <shadabkhan2910@gmail.com>
2023-09-16 09:36:18 +0530

    package

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@ const API_KEY = 'AIzaSyDSuLrZ5bAW-7L6v4ueWhCZwsYXlWlrKNE'
  export const LIVE_CHAT_COUNT =20;
 
  export const YOUTUBE_VIDEO_SEARCH_API =
-	"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key="+ API_KEY + "&q=";
+	"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&key="+ API_KEY + "&q=";
 	
  
 export const YOUTUBE_API ='https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ API_KEY;
