6ae7e9dc8292936adcbccf7b675f266529fce385
shadab khan <shadabkhan2910@gmail.com>
2023-10-23 12:21:51 +0530

    search result responsive

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -18,33 +18,33 @@ const SearchResults = () => {
 		const json = await data.json();
 		// console.log(json.items);
 		setVideos(json.items);
-        console.log(json.items)
+		console.log(json.items)
 	};
 	if (videos.length === 0) {
 		return <div>Loading...</div>;
 	}
 	return (
-		<div className=" flex flex-col md:ml-24 mt-20">
+		<div className=" md:w-auto  flex flex-col md:ml-24 mt-20  overflow-x-hidden">
 			{videos.map((video) => {
 				if (video.id.kind === "youtube#video") {
 					return (
 						<Link to={"/watch?v=" + video.id.videoId} key={video.id.videoId}>
 							{/* <Videos info={video} /> */}
-                            <div className=' p-2 m-2  h-[120px] md:flex items-top md:mb-24 mb-80 '>
-                        <div className=''>
-                            <img className='rounded-lg w-[368px] h-[201px] ' alt='thumbnail' src={video?.snippet?.thumbnails?.medium?.url} />
-                        </div>
-                        <div className='md:ml-5 md:w-[600px] mt-3 md:mt-0'>
-                            <ul>
-                                <li className='text-[20px] font-bold '>{video?.snippet?.title}</li>
-                                <li className="text-[16px] ">{video?.snippet?.channelTitle}</li>
-                                <li>{video?.snippet?.description}</li>
-                                {/* <li className='text-[14px]'>{video?.statistics?.viewCount} views</li> */}
+							<div className=' p-2 m-2  h-[120px] md:flex items-top md:mb-24 mb-80'>
+								<div className=''>
+									<img className='rounded-lg md:w-[368px] h-[201px] w-screen ' alt='thumbnail' src={video?.snippet?.thumbnails?.medium?.url} />
+								</div>
+								<div className='md:ml-5 md:w-[600px] w-screen  mt-3 md:mt-0 '>
+									<ul className=""> 
+										<li className='text-[20px] font-bold '>{video?.snippet?.title}</li>
+										<li className="text-[16px]">{video?.snippet?.channelTitle}</li>
+										<li className="md:w-auto w-[400px]">{video?.snippet?.description}</li>
+										{/* <li className='text-[14px]'>{video?.statistics?.viewCount} views</li> */}
 
-                            </ul>
-                        </div>
+									</ul>
+								</div>
 
-                    </div>
+							</div>
 						</Link>
 					);
 				}
