d5ce552b16acdf9d60c463f34c5f164290fb95d5
shadab khan <shadabkhan2910@gmail.com>
2024-07-03 21:35:39 +0530

    cors

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -8,7 +8,7 @@ const API_KEY = 'AIzaSyDjZ4msQX59QcxDcnqDkBU4e_-cJNPlvJ4'
  
 export const YOUTUBE_API ='https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ API_KEY;
 
- export const SUGGESTION_API ="https://corsproxy.io?https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+ export const SUGGESTION_API ="https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fsuggestqueries.google.com%2Fcomplete%2Fsearch%3Fclient%3Dfirefox%26ds%3Dyt%26q%3D"
 
  
 export const YOUTUBE_VIDEO_INFO_API =
